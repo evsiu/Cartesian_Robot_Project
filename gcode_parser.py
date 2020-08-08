601cb27b0cbd419a82f4eb757e2f0afe6daeb1a3
Edison <evsiu@usc.edu>
2020-08-08 05:11:50 -0700

    Updates

--- gcode_parser.py
+++ gcode_parser.py
@@ -1,20 +1,31 @@
-def parse(input_file):
-    #parse the input file
-    ifile = open(input_file, "r")
-    for line in ifile:
-        parameters = line.split(" ")
+class Parse:
+    def __init__(self, input_file):
+        self.input_file = input_file
 
-        gcode_check = parameters[0]
-        x = parameters[1][1:]
-        y = parameters[2][1:]
-        z = parameters[3][1:]
+        self.x = 0
+        self.y = 0
+        self.z = 0
 
-        feed_rate = parameters[4][1:]
-        extrude = parameters[5][1:]
-        print("X: " + x + " Y: " + y + " Z: " + z + " Feed Rate: " + feed_rate + " Extrude: " + extrude)
+        self.feed_rate = 0
+        self.extrude = 0
+    def parse(self):
+        #parse the input file
+        ifile = open(self.input_file, "r")
+        for line in ifile:
+            parameters = line.split(" ")
 
-def main():
-    input_file = "test.txt"
-    parse(input_file)
+            gcode_check = parameters[0]
+            self.x = parameters[1][1:]
+            self.y = parameters[2][1:]
+            self.z = parameters[3][1:]
 
-main()
+            self.feed_rate = parameters[4][1:]
+            self.extrude = parameters[5][1:]
+            print("X: " + self.x + " Y: " + self.y + " Z: " + self.z + " Feed Rate: " + self.feed_rate + " Extrude: " + self.extrude)
+
+#def main():
+#    input_file = "test.txt"
+#    gcode = Parse(input_file)
+#    gcode.parse()
+#
+#main()
