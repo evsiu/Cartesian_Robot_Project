c5059bd4a46ff09a43292fc009d87bdfa8a1b298
Edison <evsiu@usc.edu>
2020-08-08 05:42:43 -0700

    Updates

--- gcode_parser.py
+++ gcode_parser.py
@@ -10,8 +10,9 @@ class GCODE_PARSE:
         self.extrude = 0
     def parse(self):
         #parse the input file
-        
+
         ifile = open(self.input_file, "r")
+        self.commands = []
         for line in ifile:
             parameters = line.split(" ")
 
@@ -22,11 +23,16 @@ class GCODE_PARSE:
 
             self.feed_rate = parameters[4][1:]
             self.extrude = parameters[5][1:]
-            print("X: " + self.x + " Y: " + self.y + " Z: " + self.z + " Feed Rate: " + self.feed_rate + " Extrude: " + self.extrude)
+            command_arguments = [self.x, self.y, self.z, self.feed_rate, self.extrude]
+            #print("X: " + self.x + " Y: " + self.y + " Z: " + self.z + " Feed Rate: " + self.feed_rate + " Extrude: " + self.extrude)
+            self.commands.append(command_arguments)
 
 #def main():
 #    input_file = "test.txt"
 #    gcode = GCODE_PARSE(input_file)
 #    gcode.parse()
+#
+#    for i in range(len(gcode.commands)):
+#        print("X: " + gcode.commands[i][0] + " Y: " + gcode.commands[i][1] + " Z: " + gcode.commands[i][2] + " Feed Rate: " + gcode.commands[i][3] + " Extrude: " + gcode.commands[i][4])
 #
 #main()
