97d507a5e786bf07c1f513399663152f39d3dbea
Edison <evsiu@usc.edu>
2020-09-05 16:14:16 -0700

    Updates

--- gcode_parser.py
+++ gcode_parser.py
@@ -13,8 +13,18 @@ class GCODE_PARSE:
 
         ifile = open(self.input_file, "r")
         self.commands = []
+        self.conditions = []
+
+        self.conditions.append(str(self.input_file))
+        for param_line in ifile:
+            if(param_line[0:10] == ";Generated"):
+                break
+            else:
+                self.conditions.append(param_line[1:])
+
         for line in ifile:
             parameters = line.split(" ")
+            self.g_command = "none"
             self.x, self.y, self.z, self.feed_rate, self.extrude = "0", "0", "0", "0", "0"
             for command in parameters:
                 if(command[0] == "G"):
@@ -38,7 +48,11 @@ def main():
     gcode = GCODE_PARSE(input_file)
     gcode.parse()
 
+    if(len(gcode.conditions) > 1):
+        #for i in range(len(gcode.conditions)):
+        print(gcode.conditions[0] + "\n" + gcode.conditions[2] + " " + gcode.conditions[3] + " " + gcode.conditions[4] + "\n" + gcode.conditions[5] + "\t" + gcode.conditions[8] + "\n" + gcode.conditions[6] + "\t" + gcode.conditions[9] + "\n" + gcode.conditions[7] + "\t" + gcode.conditions[10] + "\n")
     for i in range(len(gcode.commands)):
         print(gcode.commands[i][0] + " X: " + gcode.commands[i][1] + " Y: " + gcode.commands[i][2] + " Z: " + gcode.commands[i][3] + " Feed Rate: " + gcode.commands[i][4] + " Extrude: " + gcode.commands[i][5])
+    
 
 main()
